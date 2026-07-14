0c84f145ec71fb106133403cb6bd292d76f01498
Meerib Naeem <meeribnaeem@gmail.com>
2026-07-14 22:44:33 +0400

    Fix riddle answer display on game over

--- riddle.py
+++ riddle.py
@@ -84,9 +84,11 @@ def answer(file):
             st.session_state.tries -= 1
 
             if st.session_state.tries == 0:
-                st.write(f'The correct answer was {file["answer"]}')
+                st.error(f"The correct answer was {file["riddle"]}")
+            
                 st.session_state.tries = 3
-                #st.session_state[f"answer_{st.session_state.current}"] = ""
+                if st.button("Next riddle"):
+                    st.rerun()
                 return False
     
     return None
